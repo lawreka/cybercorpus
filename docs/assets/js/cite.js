c21b8964c6da8178a10fabe60a51bbbc49f066b1
Kathryn Lawrence <klaw@MacBook-Pro.local>
2019-10-10 23:50:29 +0200

    a little css and fixing footnote hiding

--- docs/assets/js/cite.js
+++ docs/assets/js/cite.js
@@ -4,4 +4,16 @@ function showCitation(el){
 	window.addEventListener("click", function(){
 		note.setAttribute("style", "display:none;");
 	}, once=true);
+	var x = document.createElement("a");
+	x.innerHTML = "x";
+	x.setAttribute("style", "float:right;color:#fff;")
+	x.setAttribute("onclick", "hideCitation(this)");
+	note.append(x);
+}
+
+function hideCitation(el){
+	var citation = el.parentElement;
+	var footnote = el.parentElement.parentElement;
+	footnote.setAttribute("onclick", "");
+	citation.setAttribute("style", "display:none;");
 }
--- website/pages/en/about.js
+++ website/pages/en/about.js
@@ -15,7 +15,7 @@ function About(props) {
 	    <div className="docMainWrapper wrapper">
 	      <Container className="mainContainer">
 	        <div className="intro">
-            <span className="oblique">Cyber Corpus</span> was my final thesis to graduate Reed College in Portland, OR, with an art history degree in May of 2014. Eventually I'd like to link this document with the Virtual Thesis Tower at the Reed library, but for now please enjoy reading my undergraduate adventures in cyberfeminist art history (unedited from the print edition, some footnotes have been re-numbered for clarity).
+            <span className="oblique">Cyber Corpus</span> was my final thesis to graduate Reed College in Portland, OR, with an art history degree in May of 2014. Eventually I'd like to link this document with the <a href="https://library.reed.edu/etheses/">Virtual Thesis Tower</a> at the Reed library, but for now please enjoy reading my undergraduate adventures in cyberfeminist art history (unedited from the print edition, some footnotes have been re-numbered for clarity).
           </div>
 				</Container>
 			</div>
